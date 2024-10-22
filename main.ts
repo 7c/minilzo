004cb68446b0be656d5d4bdca32f47f6ad4f1f06
Your Name <you@example.com>
2024-10-22 12:19:29 -0400

    .

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
 
+const basePath = new URL('.', import.meta.url).pathname;
+export async function ensureBinariesExist() {
+  try {
+    await Deno.stat(`${basePath}bin/minilzo-arm64.so`);
+    await Deno.stat(`${basePath}bin/minilzo-x86_64.so`);
+    await Deno.stat(`${basePath}bin/minilzo-armv7l.so`);
+    console.log('Binaries are available.');
+  } catch (error) {
+    console.error('Error: Some binaries are missing!', error);
+  }
+}
 export function libPath(): string {
   let libPath: string;
   let arch: string = Deno.build.arch
@@ -6,7 +17,7 @@ export function libPath(): string {
   if (arch === "aarch64") arch = "arm64";
 
   // Use import.meta.url to get the current directory of the module
-  const basePath = new URL('.', import.meta.url).pathname;
+  
 
   // Determine the correct path to the binary
   switch (os) {
@@ -27,3 +38,5 @@ export function libPath(): string {
 }
 
 export * from "./Minilzo.ts";
+
+ensureBinariesExist();
